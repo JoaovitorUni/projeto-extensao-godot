700fdac3e3ceedf59c575dae77ad7eee1ce73e0b
João Vitor <joaovitordsimao@edu.unifil.br>
2026-04-18 13:24:04 -0300

    fix: can_place function only must return true if the position is within the grid

--- src/levels/street/scripts/grid_manager.gd
+++ src/levels/street/scripts/grid_manager.gd
@@ -3,16 +3,23 @@ class_name GridManager
 
 var _placed_objects: Dictionary = {}
 
+func is_within_grid(global_pos: Vector2) -> bool:
+	var local_pos = to_local(global_pos)
+	var cell = local_to_map(local_pos)
+	var used_rect: Rect2i = get_used_rect()
+	return used_rect.has_point(cell)
+
 func get_grid_cell(cursor_position: Vector2) -> Vector2i:
 	return local_to_map(cursor_position)
 
 func can_place(cursor_position: Vector2) -> bool:
-	var cell = get_grid_cell(cursor_position)
-	if _placed_objects.has(cell):
-		return false
-	return true
+	if is_within_grid(cursor_position):
+		var cell = get_grid_cell(cursor_position)
+		if not _placed_objects.has(cell):
+			return true
+	return false
 
-func get_cel_position(cursor_position: Vector2) -> Vector2:
+func get_cell_position(cursor_position: Vector2) -> Vector2:
 	var cell = get_grid_cell(cursor_position)
 	return map_to_local(cell)
 
