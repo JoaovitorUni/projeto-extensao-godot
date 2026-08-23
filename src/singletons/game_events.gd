f7de9827658519db6a8ae524d0c9e966307bd097
João Vitor <joaovitordsimao@edu.unifil.br>
2026-08-23 19:32:17 -0300

    fix: remove dead towers of grid

--- src/singletons/game_events.gd
+++ src/singletons/game_events.gd
@@ -4,6 +4,7 @@ signal tower_grabbed(data: TowerData)
 signal tower_placed(data: TowerData)
 signal tower_dropped()
 signal tower_purchased(cost: int)
+signal tower_died(tower_node: Node2D)
 
 signal tower_purchase_requested(tower_data: TowerData)
 signal tower_purchase_approved(tower_data: TowerData)
