5e024538cdad533fd88381aa6253336eb53ec610
João Vitor <joaovitordsimao@edu.unifil.br>
2026-08-23 16:01:03 -0300

    feat: implement a basic projectile

--- src/components/combat/hitbox_component.gd
+++ src/components/combat/hitbox_component.gd
@@ -1,11 +1,23 @@
 extends Area2D
 class_name HitboxComponent
 
+signal hit_applied(hurtbox: HurtboxComponent)
+
 @export var damage: float = 10.0
+@export var faction: GameLayers.Faction = GameLayers.Faction.TOWER
 
 func _ready() -> void:
+	match faction:
+		GameLayers.Faction.TOWER:
+			collision_layer = GameLayers.LAYER_TOWER_ATTACK
+			collision_mask = GameLayers.LAYER_ENEMY_HURTBOX
+		GameLayers.Faction.ENEMY:
+			collision_layer = GameLayers.LAYER_ENEMY_ATTACK
+			collision_mask = GameLayers.LAYER_TOWER_HURTBOX
+
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area: Area2D) -> void:
 	if area is HurtboxComponent:
 		area.take_damage(damage)
+		hit_applied.emit(area)
---
+++ src/towers/definitions/shooter_tower_data.gd
@@ -0,0 +1,6 @@
+extends TowerData
+class_name ShooterTowerData
+
+@export_category("Shooter Config")
+@export var projectile_scene: PackedScene
+@export var shoot_cooldown: int
