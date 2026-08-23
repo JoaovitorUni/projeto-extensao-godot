a3d9f9b6c65eedf2942a283ec2e58bf788accf3b
João Vitor <joaovitordsimao@edu.unifil.br>
2026-08-23 17:54:18 -0300

    refactor: abstracts debug to singleton

--- src/levels/street/scripts/main.gd
+++ src/levels/street/scripts/main.gd
@@ -8,11 +8,6 @@ extends Node2D
 @export var shop_bar_scene: PackedScene
 @export var currency_counter_scene: PackedScene
 
-@export_category("Debug")
-@export var debug_tower_events: bool = false
-@export var debug_economy_events: bool = false
-@export var debug_wave_events: bool = false
-
 @onready var wave_manager = $WaveManager
 
 var _grabbed_tower: bool = false
@@ -21,12 +16,6 @@ func _ready() -> void:
 	GameLayers.setup($Game, $UI, $Overlay, %TowersContainer, %EnemiesContainer, %ProjectilesContainer)
 	GameEvents.tower_purchase_approved.connect(_on_tower_purchase_approved)
 	GameEvents.tower_grabbed.connect(_on_tower_grabbed)
-	if debug_tower_events:
-		_set_debug_tower_events_handlers()
-	if debug_economy_events:
-		_set_debug_economy_events_handlers()
-	if debug_wave_events:
-		_set_debug_wave_events_handlers()
 	_build_level_ui()
 
 	wave_manager.start_level()
@@ -50,42 +39,7 @@ func _on_tower_dropped() -> void:
 	_grabbed_tower = false
 	GameEvents.tower_dropped.emit()
 
-func _set_debug_tower_events_handlers() -> void:
-	GameEvents.tower_grabbed.connect(func(tower_data: TowerData):
-		print("Evento: tower_grabbed. TowerData: %s." % tower_data.name)
-	)
-	GameEvents.tower_placed.connect(func(tower_data: TowerData):
-		print("Evento: tower_placed. TowerData: %s." % tower_data.name)
-	)
-	GameEvents.tower_dropped.connect(func(): print("Evento: tower_dropped."))
 
-func _set_debug_economy_events_handlers() -> void:
-	GameEvents.tower_purchase_requested.connect(func(tower_data: TowerData):
-		print("Evento: tower_purchase_requested. TowerData: %s." % tower_data.name)
-	)
-	GameEvents.tower_purchase_approved.connect(func(tower_data: TowerData):
-		print("Evento: tower_purchase_approved. TowerData: %s." % tower_data.name)
-	)
-	GameEvents.tower_purchase_denied.connect(func(tower_data: TowerData):
-		print("Evento: tower_purchase_denied. TowerData: %s." % tower_data.name)
-	)
-	GameEvents.currency_collected.connect(func(value: int):
-		print("Evento: currency_collected. Value: %d." % value)
-	)
-	GameEvents.currency_changed.connect(func(new_currency: int):
-		print("Evento: currency_changed. New Currency: %d." % new_currency)
-	)
-
-func _set_debug_wave_events_handlers() -> void:
-	GameEvents.wave_started.connect(func(wave_index: int, is_flag_wave: bool):
-		print("Evento: wave_started. Wave Index: %s, Flag Wave: %s." % [wave_index, is_flag_wave])
-	)
-	GameEvents.wave_completed.connect(func(wave_index: int):
-		print("Evento: wave_completed. Wave Index: %s." % wave_index)
-	)
-	GameEvents.all_waves_completed.connect(func(tower_data: TowerData):
-		print("Evento: all_waves_completed. TowerData: %s." % tower_data.name)
-	)
 
 # TODO: Componentizar construção da UI.
 func _build_level_ui() -> void:
